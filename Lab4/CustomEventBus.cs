ef7a9f39d2c00effad759e6f076a5fa932579d0c
MartinyukLabs <I.Martynyuk_FIT_2_21_B_d@knute.edu.ua>
2023-03-18 22:59:49 +0200

    Initial commit

---
+++ Lab4/CustomEventBus.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class CustomEventBus
+    {
+        private Dictionary<Type, List<Delegate>> eventHandlers = new Dictionary<Type, List<Delegate>>();
+        private Dictionary<Type, DateTime> lastEventTimes = new Dictionary<Type, DateTime>();
+        private int throttleInterval;
+        public CustomEventBus(int throttleInterval)
+        {
+            this.throttleInterval = throttleInterval;
+        }
+        public void RegisterHandler<TEvent>(Action<TEvent> handler)
+        {
+            Type eventType = typeof(TEvent);
+            if (!eventHandlers.ContainsKey(eventType))
+            {
+                eventHandlers[eventType] = new List<Delegate>();
+                lastEventTimes[eventType] = DateTime.MinValue;
+            }
+            eventHandlers[eventType].Add(handler);
+        }
+        public void UnregisterHandler<TEvent>(Action<TEvent> handler)
+        {
+            Type eventType = typeof(TEvent);
+            if (eventHandlers.ContainsKey(eventType))
+                eventHandlers[eventType].Remove(handler);
+        }
+        public void Dispatch<TEvent>(TEvent ev)
+        {
+            Type eventType = typeof(TEvent);
+            DateTime lastEventTime = lastEventTimes[eventType];
+            DateTime currentTime = DateTime.Now;
+            TimeSpan elapsedTime = currentTime - lastEventTime;
+            if (elapsedTime.TotalMilliseconds < throttleInterval)
+                return;
+            lastEventTimes[eventType] = currentTime;
+            if (eventHandlers.ContainsKey(eventType))
+                foreach (Delegate handler in eventHandlers[eventType])
+                    if (handler is Action<TEvent> typedHandler)
+                        typedHandler(ev);
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            First();
+        }
+        public static void First()
+        {
+            CustomEventBus eventBus = new CustomEventBus(1000);
+            eventBus.RegisterHandler<string>(s => Console.WriteLine("Received string event: " + s));
+            eventBus.RegisterHandler<int>(i => Console.WriteLine("Received int event: " + i));
+
+            eventBus.Dispatch("hello");
+            eventBus.Dispatch(42);
+            Thread.Sleep(1000);
+            eventBus.Dispatch("world");
+            eventBus.Dispatch(123);
+            Thread.Sleep(1000);
+            eventBus.Dispatch("foo");
+            eventBus.Dispatch(99);
+
+            eventBus.UnregisterHandler<int>(i => Console.WriteLine("Received int event: " + i));
+        }
+        public static void Second()
+        {
+
+        }
+    }
+}
